1ba0d306459a35425ab18a3ff05db0d511133df5
letfly <letflykid@gmail.com>
2016-06-09 10:54:09 +0800

    practice

--- Java/test/day02/OperateDemo2.py
+++
@@ -1,6 +0,0 @@
-x=4;y=6
-print "x="+str(x),"y="+str(y)
-x=x^y
-y=x^y
-x=x^y
-print "x="+str(x),"y="+str(y)
--- Java/test/day02/SwitchDemo.py
+++
@@ -1,9 +0,0 @@
-num=6
-if num==4:
-	print 'a'
-elif num==6:
-	print 'b'
-elif num==2:
-	print 'c'
-else:
-	print 'd'
