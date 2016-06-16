cb80c48e3703b9cbf49866efc811330c89f90195
letfly <letflykid@gmail.com>
2016-06-16 19:40:34 +0800

    remove-element

---
+++ Python/algorithms/leetcode/remove_element.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def remove_element(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        nums[:] = filter(lambda x: x!= val, nums)
+        return len(nums)
+
+s = Solution()
+print s.remove_element([3,2,2,3], 3)
