f82d5c3726277e818b65d3202b32d98a2daa44ba
letfly <letflykid@gmail.com>
2016-06-21 11:09:56 +0800

    same-tree

--- Python/algorithms/leetcode/longest_valid_parentheses.py
+++ Python/algorithms/leetcode/longest_valid_parentheses.py
@@ -14,15 +14,18 @@ class Solution(object):
                     lst=-1
                 else:
                     stk.append(i)
+                print stk
             else:
                 if stk:
                     stt=stk.pop()
+                    print stt
                     if i-stt+1>result:
                         result=i-stt+1
+                        print result
                     lst=stt
                 else:
                     lst=-1
         return result
 
 s = Solution()
-print s.longestValidParentheses(")()())")
+print s.longestValidParentheses("((()())")
