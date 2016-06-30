c0244348aaae1a3c8937dbbc76c575d342e1d659
letfly <letflykid@gmail.com>
2016-06-30 17:26:36 +0800

    binary-tree-preorder-traversal clone-graph

---
+++ Python/algorithms/leetcode/binary_tree_preorder_traversal.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        ret = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                ret.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+        return ret
+
+s = Solution()
+print s.preorderTraversal()
