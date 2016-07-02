1bba7a3fdb14ed9fbd5f36db9df0ca3831710eb2
letfly <letflykid@gmail.com>
2016-07-02 16:55:36 +0800

    search-in-rotated-sorted-array

---
+++ Python/algorithms/leetcode/search_in_rotated_sorted_array.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        lo, hi = 0, len(nums) - 1
+        while lo < hi:
+            mid = (lo+hi)/2
+            if(nums[0] > target) ^ (nums[0] > nums[mid]) ^ (target > nums[mid]):
+                lo = mid + 1
+            else:
+                hi = mid
+        return lo if target in nums[lo: lo+1] else -1
+
+s = Solution()
+print s.search([4,5,6,2,3], 3)
